c0a74d1fb5e6c14469875a4a98c843214c67cc70
phillipsl2atwit <phillipsl2@wit.edu>
2022-09-23 17:57:27 -0400

    finished

--- comp1000_pa1/src/comp1000_pa1/PA1a.java
+++ comp1000_pa1/src/comp1000_pa1/PA1a.java
@@ -1,5 +1,24 @@
 package comp1000_pa1;
 
-public class PA1a {
+import java.util.*;
 
+public class PA1a {
+	public static void main(String[] args) 
+	{
+		int inches = 0;
+	    Scanner stdin = new Scanner(System.in);
+	    
+	    System.out.print("Enter number of yards: ");
+		inches += 36*Integer.parseInt(stdin.nextLine());
+		
+	    System.out.print("Enter number of feet: ");
+		inches += 12*Integer.parseInt(stdin.nextLine());
+		
+	    System.out.print("Enter number of inches: ");
+		inches += Integer.parseInt(stdin.nextLine());
+		
+		System.out.println("Total number of inches: " + String .valueOf(inches));
+		
+		stdin.close();
+	}   
 }
--- comp1000_pa1/src/comp1000_pa1/PA1b.java
+++ comp1000_pa1/src/comp1000_pa1/PA1b.java
@@ -1,8 +1,20 @@
 package comp1000_pa1;
 
-public class PA1b {
-	public static void main(String[] args) {
-		// your code here
-	}
+import java.util.*;
 
+public class PA1b {
+	public static void main(String[] args) 
+	{
+		int inches = 0;
+	    Scanner stdin = new Scanner(System.in);
+	    
+	    System.out.print("Enter number of inches: ");
+		inches = Integer.parseInt(stdin.nextLine());
+		
+	    System.out.println("Yards: " +(int) Math.floor(inches/36));
+	    System.out.println("Feet: " + (int) Math.floor((inches%36)/12));
+	    System.out.println("Inches: " + (inches%36)%12);
+	    
+	    stdin.close();
+	}   
 }
